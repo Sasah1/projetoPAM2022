cfae1bfffd23bd28b7c4369b69ad7c15e3ca2538
MilleneSilva <114960641+MilleneSilva@users.noreply.github.com>
2022-10-17 20:07:48 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String media = "";
   String status = "";
   String teste = "";
+  String teste2 = "";
 
   @override
   Widget build(BuildContext context) {
